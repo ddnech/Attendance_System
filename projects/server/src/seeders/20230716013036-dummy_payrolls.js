d772b657b885a3f7d336ab306743f13af624bc98
ddnech <ddnech@gmail.com>
2023-07-16 21:06:07 +0700

    update seeders file

--- projects/server/src/seeders/20230716013036-dummy_payrolls.js
+++ projects/server/src/seeders/20230716013036-dummy_payrolls.js
@@ -8,24 +8,21 @@ module.exports = {
     let dummyPayrolls = [];
     let currentDate = dayjs();
 
-    // Creating dummy data for 18 months
     for (let i = 0; i < 18; i++) {
       let payrollMonth = currentDate.subtract(i, 'month');
 
-      // Create payroll for user id 2 and 4
-      [2, 4].forEach(userId => {
+      [2, 3].forEach(userId => {
         dummyPayrolls.push({
           user_id: userId,
-          date: payrollMonth.toDate(), // Use date field to store payroll date
-          amount: chance.integer({min: 2000, max: 3000}), // random amount between 2000 and 3000
-          deductions: chance.integer({min: 0, max: 500}), // random deduction up to 500
+          date: payrollMonth.toDate(), 
+          amount: chance.integer({min: 2000, max: 3000}),
+          deductions: chance.integer({min: 0, max: 500}),
           createdAt: new Date(),
           updatedAt: new Date()
         });
       });
     }
-
-    // Insert the dummyPayrolls array into the Payrolls table
+    
     return queryInterface.bulkInsert('Payrolls', dummyPayrolls, {});
   },
 
